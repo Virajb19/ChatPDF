f4c8b6fb8f25298072af0779507f5457acfebbe7
Virajb19 <virajb853@gmail.com>
2024-10-23 20:25:08 +0530

    v4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
 'use client'
+import { useSession } from 'next-auth/react'
+import Link from 'next/link'
 import Avatar from 'react-avatar'
+import { FileUpload } from '~/components/ui/file-upload'
+import { LogIn, ArrowRightToLine } from 'lucide-react';
+import { useState } from 'react'
 
 export default function HomePage() {
-    return <main className="w-full min-h-screen flex-center bg-black text-white">
-               <div className="flex flex-col p-1 gap-1">
+
+ const { data } = useSession()
+ const isAuth = !!data
+
+ const [avatarURL, setAvatarURL] = useState('https://imgs.search.brave.com/L3QH_L8tiQCkvsAiK-xmjN_tlyqcBzdBdRAs8RHlNmU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA3LzAzLzg2LzEx/LzM2MF9GXzcwMzg2/MTExNF83WXhJUG5v/SDhOZm1ieUVmZk96/aWFYeTBFTzFOcFJI/RC5qcGc')
+
+
+    return <main className="w-full min-h-screen flex-center bg-gradient-to-r from-rose-200 to-teal-200 text-black">
+               <div id='homepage' className="flex flex-col p-1 gap-1 items-center">
                  <div id='avatar' className='flex p-1 gap-2 items-center'>
-                  <h1 className="mb:text-3xl tb:text-c-5xl">Chat with any PDF</h1>
-                  <Avatar src='https://imgs.search.brave.com/gtfOZaAIi09umZr6NiLshAqo2QASpEFDHY85eVN5Xh4/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9kb2Nz/Lm1hdGVyaWFsLXRh/aWx3aW5kLmNvbS9p/bWcvZmFjZS0yLmpw/Zw' size='50' round />
+                  <h1 className="mb:text-3xl tb:text-c-5xl font-medium mb:tracking-tighter">Chat with any PDF</h1>
+                  {isAuth && <Avatar className='cursor-pointer' size='50' round src={avatarURL} />}
                   </div>
+                  {isAuth && <button className='flex gap-2 items-center px-4 py-2 bg-black text-white rounded-lg font-semibold hover:gap-4 transition-all'>Go to chats<span className='hover:translate-x-1 duration-200'><ArrowRightToLine /></span></button>}
+                  <p className='max-w-xl lg:text-xl mt-2'>Join millions of students, researchers and professinals to
+                  instantly anwer questions and understand research with AI</p>
+                  {isAuth ? <div className='lg:w-[60%]'><FileUpload /></div> : <Link href={'/signin'}><button className='flex gap-2 items-center px-4 py-2 mt-3 min-w-1/2 bg-black text-white rounded-lg font-semibold text-sm'>Login to get started !<span><LogIn /></span></button></Link>}
                </div>
         </main>
 }
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,50 +1,12 @@
-'use client'
+import { redirect } from "next/navigation";
+import SignIn from "~/components/SignIn";
+import { getServerAuthSession } from "~/server/auth";
 
-import { toast } from 'sonner'
-import { useForm } from 'react-hook-form'
-import { useState } from 'react'
-import { motion } from 'framer-motion'
-import { FaRegEye, FaRegEyeSlash } from "react-icons/fa"
-import { signIn } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
-
-interface SignInData {
-    email: string,
-    password: string
-}
-
-export default function Page() {
-
-    const {register, handleSubmit} = useForm<SignInData>()
-    const router = useRouter()
-
-    async function OnSubmit(data: SignInData) {
-        const result = await signIn('credentials',{email: data.email, password: data.password, redirect: false})
-        if(!result?.error) {
-            toast.success('Signed in successfully')
-            router.push('/')
-        } else toast.error(result?.error, {duration: 3000})
+export default async function SignInPage() {
+  
+    const session = await getServerAuthSession()
+    if(session?.user) {
+        redirect('/')
     }
-    
-    return <main className="w-full min-h-screen flex-center bg-black text-white">
-               <div id="signup" className="flex flex-col p-2 gap-3 rounded-xl items-center w-[30%] mb:w-[90%] tb:w-1/2 py-5 border">
-                     <h2 className="text-center mb:text-5xl tb:text-6xl">Sign in</h2>
-                     <form className="flex flex-col p-1 gap-3 w-3/4 mb:w-full items-center" onSubmit={handleSubmit(OnSubmit)}>
-                           <Input text='email' register={register}/>
-                           <Input text='password' register={register}/>
-                            <motion.button type='submit' whileHover={{scale: 1.05}} whileTap={{scale: 0.9}} 
-                            className="px-7 py-2 bg-white text-black font-bold rounded-full border border-zinc-700 cursor-pointer">Sign in</motion.button>
-                     </form>
-               </div>
-        </main>
-}
-
-function Input({text, register}: {text: string, register: any}) {
-
-    const [showPassword,setShowPassword] = useState(false)
-
-    return <div className="relative flex flex-col p-1 gap-1 w-full"> 
-      <input {...register(`${text}`)} type={text === 'password' ? (showPassword ? 'text' : 'password') : 'text'} placeholder={text} className="outline-none px-4 py-2 bg-transparent rounded-xl border focus:ring-2 focus:ring-blue-600 focus:border-transparent duration-200" />
-      {text === 'password' && <span onClick={() => setShowPassword(!showPassword)} className='absolute p-2 right-2 top-2 cursor-pointer rounded-lg hover:bg-zinc-800 duration-200 text-white'>{showPassword ? <FaRegEyeSlash /> : <FaRegEye />}</span>}
-     </div>
+    return <SignIn />
 }
